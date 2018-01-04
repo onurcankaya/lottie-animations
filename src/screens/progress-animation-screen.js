048a80451e177e9fc43b292270d476014caa0614
Onur Çankaya <onur.eren.cankaya@gmail.com>
2018-01-04 17:26:05 +0000

    implemented progress animation

--- src/screens/progress-animation-screen.js
+++ src/screens/progress-animation-screen.js
@@ -8,20 +8,41 @@ export default class ProgressAnimation extends React.Component {
     this.state = { progress: new Animated.Value(0) };
   }
 
+  fakeStep = (prev = 0) => {
+    const max = 20;
+    const randomTime = Math.random() * 2000 + 1000;
+    setTimeout(() => {
+      let next = Math.round(Math.random() * (100 - prev) + prev);
+      if (next - prev > max) {
+        next = prev + max;
+      }
+
+      let toValue = next / 100;
+      if (toValue > 0.8) {
+        toValue = 1;
+      }
+
+      console.warn(toValue);
+
+      // animate to next value
+      Animated.timing(this.state.progress, {
+        toValue,
+        duration: 1000
+      }).start();
+      if (toValue < 1) {
+        this.fakeStep(next);
+      }
+    }, randomTime);
+  };
+
   componentDidMount() {
-    Animated.timing(this.state.progress, {
-      toValue: 1,
-      duration: 4000
-    }).start();
+    this.fakeStep();
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <LottieView
-          ref={animation => {
-            this.animation = animation;
-          }}
           style={{ width: 200, height: 200, top: 70, left: 45 }}
           source={require("../images/progress-animation.json")}
           progress={this.state.progress}
--- src/screens/screen-transition-animation-screen.js
+++ src/screens/screen-transition-animation-screen.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Animated, View } from 'react-native'
-import LottieView from 'lottie-react-native'
+import React from "react";
+import { Animated, View } from "react-native";
+import LottieView from "lottie-react-native";
 
 export default class ScreenTransitionAnimation extends React.Component {
   componentDidMount() {
-    this.animation.play()
+    this.animation.play();
   }
 
   render() {
     return (
       <LottieView
-        ref={animation => {this.animation = animation}}
+        ref={animation => {
+          this.animation = animation;
+        }}
         style={{
           width: 200,
           height: 200,
           top: 60,
-          left: 45,
+          left: 45
         }}
-        source={require('../images/loading-animation.json')}
+        source={require("../images/loading-animation.json")}
         loop={true}
       />
-    )
+    );
   }
 }
