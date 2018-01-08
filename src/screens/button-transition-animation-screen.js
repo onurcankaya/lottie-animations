2217f24c97773989b1393b75047061f6584c7330
Onur Çankaya <onur.eren.cankaya@gmail.com>
2018-01-08 09:04:01 +0000

    changed button transition animation

--- src/screens/button-transition-animation-screen.js
+++ src/screens/button-transition-animation-screen.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import { View } from 'react-native'
-import AnimatedButton from '../components/animated-button'
+import React from "react";
+import { View } from "react-native";
+import AnimatedButton from "../components/animated-button";
 
 export default class ButtonAnimation extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       menuToggled: false
-    }
+    };
   }
 
   handlePress = () => {
-    this.setState({ menuToggled: !this.state.menuToggled })
-  }
+    this.setState({ menuToggled: !this.state.menuToggled });
+  };
 
   render() {
     return (
       <AnimatedButton
-        duration={5000}
+        duration={2000}
         toggled={this.state.menuToggled}
         onPress={this.handlePress}
-        fromValue={0}
-        toValue={1}
-        source={require('../images/heart-animation.json')}
+        fromValue={0.1}
+        toValue={0.6}
+        source={require("../images/button-animation.json")}
         style={{
           width: 600,
-          height: 600,
+          height: 500,
           right: 55,
-          top: -60,
+          top: -60
         }}
       />
-    )
+    );
   }
 }
